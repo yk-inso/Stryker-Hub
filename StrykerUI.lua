5a6c831e48f1765b7e06df8b3c3842dedda0c6cf
yk-inso <sleepyinsomniac91@gmail.com>
2025-02-28 20:30:11 -0500

    Update StrykerUI.lua

--- StrykerUI.lua
+++ StrykerUI.lua
@@ -1,7 +1,7 @@
-local OrionLib = loadstring(game:HttpGet(('https://github.com/yk-inso/Stryker-Hub/blob/902b1b4f9f72988ba42d5baf0e22d6c142de38eb/StrykerHub.lua')))()
-local Window = OrionLib:MakeWindow({Name = "Stryker Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Stryker-Hub-main" , IntroText = "Welcome to Stryker Hub"})
+local StrykerLib = loadstring(game:HttpGet(('https://github.com/yk-inso/Stryker-Hub/blob/902b1b4f9f72988ba42d5baf0e22d6c142de38eb/StrykerHub.lua')))()
+local Window = StrykerLib:MakeWindow({Name = "Stryker Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Stryker-Hub-main" , IntroText = "Welcome to Stryker Hub"})
 
-OrionLib:MakeNotification({
+StrykerLib:MakeNotification({
 	Name = "Alpha 1",
 	Content = "This Script is currently in its Alpha Phase.",
 	Image = "rbxassetid://4483345998",
